c0e9b0978676e54adc0a23e48945262939256c24
Naomi <8650521+de-ocampo@users.noreply.github.com>
2020-03-22 15:28:08 -0700

    Stop printing export button

--- src/Components/LayOut.js
+++ src/Components/LayOut.js
@@ -1,49 +1,54 @@
 import React from 'react'
-import { Layout, Divider  } from 'antd';
+import {Layout, Divider} from 'antd';
 import FormDev from "../FormComponents/FormDev";
 import FormSym from "../FormComponents/FormSym";
 import FormSubmit from "../FormComponents/FormSubmit";
 
-const { Header, Footer, Content } = Layout;
-
+const {Header, Footer, Content} = Layout;
 
 
 /*this file handles the overall layout of the website*/
 
 class LayOut extends React.Component {
-    constructor(props) {
+  constructor(props) {
 
-        super(props);
-        this.state = {
-            current: 0,
-        };
-    }
+    super(props);
+    this.state = {
+      current: 0,
+    };
+  }
 
-    render() {
-        return (
-          <div id="form-content-to-pdf">
-            <Layout>
-                {/*header styling*/}
-                <Header style={{ zIndex: 1, width: '100%', height: '85px'}}>
-                    <h1 style = {{color: 'white'}}>Melmed Center</h1>
-                </Header>
-              <Content>
-                <div className="site-layout-background" style={{paddingLeft: 80, paddingTop: 40, paddingRight: 80, paddingBottom: 50, minHeight: 380}}>
-
-                  <div style = {{paddingLeft: '20px'}} id="form-content-to-pdf">
-                      <FormDev/>
-                      <Divider/>
-                      <FormSym/>
-                      <Footer style={{textAlign: 'center'}}>4848 E. Cactus Rd. Ste. #940, Scottsdale, AZ 85254 | Phone (480) 443-0050 | Fax (480) 443-4018 | Toll Free 877-587-1770</Footer>
-                  </div>
-                </div>
-              </Content>
-                <FormSubmit/>
-
-            </Layout>
-          </div>
-        )
-    }
+  render() {
+    return (
+      <Layout>
+        <div id="form-content-to-pdf">
+          {/*header styling*/}
+          <Header style={{zIndex: 1, width: '100%', height: '85px'}}>
+            <h1 style={{color: 'white'}}>Melmed Center</h1>
+          </Header>
+          <Content>
+            <div className="site-layout-background" style={{
+              paddingLeft: 80,
+              paddingTop: 40,
+              paddingRight: 80,
+              paddingBottom: 50,
+              minHeight: 380
+            }}>
+              <FormDev/>
+              <Divider/>
+              <FormSym/>
+              <div style={{textAlign: 'center'}}>4848 E. Cactus Rd. Ste. #940, Scottsdale, AZ
+                85254 | Phone (480) 443-0050 | Fax (480) 443-4018 | Toll Free 877-587-1770
+              </div>
+            </div>
+          </Content>
+        </div>
+        <Footer>
+          <FormSubmit/>
+        </Footer>
+      </Layout>
+    )
+  }
 }
 
 
